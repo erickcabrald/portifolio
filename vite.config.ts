c51522e71041a1432e0823d086fbd95fd2bc97fa
cabral <erick.cabral.dutraa@gmail.com>
2026-02-07 17:18:21 -0300

    deploy 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"; // ou o plugin que você usa
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths"; // Adicione isso
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio/", // O nome do repositório deve estar entre barras
+  plugins: [react(), tsconfigPaths()], // Adicione aqui
+  base: "/portfolio/",
 });
